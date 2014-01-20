42539d96675d2d4867f94cd4c004879301a7a50c
thomas <thomas@tower>
2014-01-20 01:34:12 +0100

    add splashscreen

--- android/src/com/friendlyvillagers/gdxclojure/LibGDXActivity.java
+++ android/src/com/friendlyvillagers/gdxclojure/LibGDXActivity.java
@@ -1,17 +1,40 @@
 package com.friendlyvillagers.gdxclojure;
 
-import android.app.Activity;
 import android.os.Bundle;
-import com.badlogic.gdx.backends.android.AndroidApplication;
-import clojure.gdx.MyGame;
+import android.app.Activity;
+import android.content.Intent; 
+import android.os.Handler;
+import android.view.Window;
 
-public class LibGDXActivity extends AndroidApplication
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-	initialize(new MyGame(), true);
-    }
+public class LibGDXActivity extends Activity {
+
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    requestWindowFeature(Window.FEATURE_NO_TITLE); 
+    setContentView(R.layout.main);
+    
+    Handler h = new Handler();
+    Runnable r = new Runnable(){
+	public void run()
+	{
+	  try {
+	    Class.forName("clojure.lang.RT");
+	  } catch (Exception e)
+	    {		
+	      System.out.println("clojure.lang.RT not found!");
+	    }		
+	  Intent intent = new Intent(LibGDXActivity.this, GameActivity.class);
+	  startActivityForResult(intent,0);  
+	}};
+	h.postDelayed(r,50);
+  }  
+
+ @Override
+ protected void onActivityResult(int requestCode, int resultCode, Intent data)
+  {
+    finish();
+  }
 }
